d4c0e132f4e1154ca2463694c8888c84f6c6c8af
RaksRaks <mraksunator@gmail.com>
2024-11-09 23:57:03 +0200

    Update Students.cs

--- DataLayer/Students.cs
+++ DataLayer/Students.cs
@@ -8,5 +8,20 @@ namespace PRG282_Project_StudentSystem.DataLayer
 {
     internal class Students
     {
-    }
+                     public string Name { get; set; }
+             public string LastName { get; set; }
+            
+             public int StudentId { get; set; }
+            
+             public string Course { get; set; }
+            
+             public Students(string name, string lastName, int id, string course)
+             {
+                 this.Name = name;
+                 this.LastName = lastName;
+                 this.StudentId = id;
+                 this.Course = course;
+            
+             }
+                }
 }
